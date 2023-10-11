529060bf9506395a32f580cd85e48d96f761fd5d
pedrogamedev <pedrobrunigavioli@gmail.com>
2023-10-11 15:49:38 -0300

    Some things
    - documentation of js
    - started the checkbox part

--- script.js
+++ script.js
@@ -6,39 +6,49 @@ const btnSend = document.getElementById("sendData");
 
 //constructor do object book
 
-function Book(nome, ano)
+function Book(nome, ano, read)
 {
     this.nome = nome;
     this.ano = ano;
+    this.read = read;
 }
 
 
+// Botao para abrir forms
 btnForms.addEventListener('click', ()=>
 {
     document.getElementById("forms").style.visibility="visible";
   
 });
 
+// Botao para fechar forms
 btnClose.addEventListener('click', ()=>
 {
     document.getElementById("forms").style.visibility="hidden";
 });
 
+// Botao para mandar os dados
 btnSend.addEventListener('click', ()=>
 {
-    if(validateForms())
+    if(GetForms())
     {
         document.getElementById("forms").style.visibility="hidden";
-        addBook(nome, num);
+        addBook(nome, num, read);
     }
 });
 
-
-function validateForms()
+// Pegar informacoes do forms
+function GetForms()
 {
+
+    // pegando valores dos inputs
     const inputName = document.getElementById("bookName").value;
     const inputNum = document.getElementById("year").value;
+    const inputRead = document.getElementById("read").checked;
+    
+// validacao de forms
 
+    // verificando se algum livro ja tem esse nome
     for(let i = 0; i < BookList.length; i++)
     {
         if(BookList[i].nome == inputName)
@@ -48,39 +58,48 @@ function validateForms()
         }
     }
 
+    // verificando se o nome tem mais que 25 caracteres
     if(inputName.length > 25)
     {
         alert("Insira um nome de ate 25 caracteres!");
         return false;
     }
+    // verificando se o nome esta vazio
     else if(inputName.length == 0)
     {
         alert("Insira um nome!");
         return false;
     }
+    // verificando se o ano tem mais que 10 caracteres
     else if(inputNum.length > 10)
     { 
         alert("Insira um ano de ate 10 caracteres!");
         return false;
     }
+    // verificando se o ano nao esta vazio
     else if(inputNum.length == 0)
     { 
         alert("Insira um ano!");
         return false;
     }
-    return true, nome = inputName, num = inputNum;
+
+    return true, nome = inputName, num = inputNum, read = inputRead;
 } 
 
-function addBook(nome, ano)
+function addBook(nome, ano, read)
 {
-    BookList.push(new Book(nome,ano));
+    BookList.push(new Book(nome,ano,read));
     updateGrid();
 }
 
+// fucao que refaz o grid, ou seja retira todos os items e 
+// repoe com a lista atualizadas
+
 function updateGrid()
 {
-
     const bookGrid = document.getElementById("bookGrid");
+
+    //limpando o grid
 
     let child = bookGrid.lastElementChild;
     while(child)
@@ -88,8 +107,13 @@ function updateGrid()
         bookGrid.removeChild(child);
         child = bookGrid.lastElementChild;
     }
+
+    // adicionando items ao grid
+
     for(i = 0; i<BookList.length; i++)
     {
+        // criando elementos
+
         const book = document.createElement("div");
         book.classList.add("block");
 
@@ -102,6 +126,8 @@ function updateGrid()
     
         bookYear.innerHTML ="Year: <br>" + BookList[i].ano;
 
+        // estilizacao de ano
+
         if(BookList[i].ano < 0)
         {
             bookYear.innerHTML ="Year<br>" +BookList[i].ano *-1 + " B.C.";
@@ -111,15 +137,30 @@ function updateGrid()
             bookYear.innerHTML ="Year: <br>0"      
         }
 
+        // estilizacao de botao de read
+
+        const readButton = document.createElement("button");
+        readButton.classList = "read";
+        if(BookList[i].read)
+        {
+            
+        }
+        else
+        {
+
+        }
+
         // colocando os tags nos seus devidos lugares
 
         book.appendChild(bookName);
         book.appendChild(bookYear);
         bookGrid.appendChild(book);
 
-        //atualizando a quantidade de livros
+        // atualizando a quantidade de livros
+
         let books = i+1;
         const bookQnt = document.getElementById("qntBooks");
+
         if(books == 1)
         {
             bookQnt.innerHTML = "There is "+ books + " book in this List";
